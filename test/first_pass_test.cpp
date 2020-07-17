24821272f0f4bbf4a24cd1c2838bde43b515216d
chrishaig14 <chrishaig14@gmail.com>
2020-07-16 22:22:06 -0300

    Improve first_pass_test.cpp with some utils functions

--- test/first_pass_test.cpp
+++ test/first_pass_test.cpp
@@ -11,6 +11,110 @@ void assert_eq_si(SimpleInfo* t1, SimpleInfo* t2) {
     }
 }
 
+bool both_null(void* a, void* b) {
+    return a == nullptr and b == nullptr;
+}
+
+bool one_null(void* a, void* b) {
+    return a == nullptr or b == nullptr;
+}
+
+bool equal(SimpleInfo* a, SimpleInfo* b) {
+    if (both_null(a, b)) return true;
+    if (one_null(a, b)) return false;
+    if (a->parent == b->parent) {
+        if (a->type_parameters.size() != b->type_parameters.size()) return false;
+        for (int i = 0; i < a->type_parameters.size(); i++) {
+            if (!equal(a->type_parameters[i], b->type_parameters[i]))return false;
+        }
+    }
+    return true;
+}
+
+bool equal(FunctionInfo* a, FunctionInfo* b) {
+    if (both_null(a, b)) return true;
+    if (one_null(a, b)) return false;
+    if (equal(a->return_type, b->return_type)) {
+        if (a->parameter_types.size() != b->parameter_types.size()) return false;
+        for (int i = 0; i < a->parameter_types.size(); i++) {
+            if (!equal(a->parameter_types[i], b->parameter_types[i]))return false;
+        }
+    }
+    return true;
+}
+
+bool equal(ClassInfo* a, ClassInfo* b) {
+    if (both_null(a, b)) return true;
+    if (one_null(a, b)) return false;
+    if (a->methods.size() != b->methods.size()) return false;
+    for (auto it : a->methods) {
+        if (b->methods.count(it.first) == 0)return false;
+        if (!equal(it.second, b->methods[it.first])) return false;
+    }
+    if (a->fields.size() != b->fields.size()) return false;
+    for (auto it : a->fields) {
+        if (b->fields.count(it.first) == 0)return false;
+        if (!equal(it.second, b->fields[it.first])) return false;
+    }
+    return true;
+}
+
+
+bool equal(SymbolInfo* a, SymbolInfo* b) {
+    if (both_null(a, b)) return true;
+    if (one_null(a, b)) return false;
+    if (a->type == b->type) {
+        switch (a->type) {
+            case SINFO::FUNCTION:
+                return equal(a->function_info, b->function_info);
+            case SINFO::CLASS:
+                return equal(a->class_info, b->class_info);
+            case SINFO::SIMPLE:
+                return equal(a->simple_info, b->simple_info);
+        }
+    }
+    return false;
+}
+
+SimpleInfo* s_info(TypeNode* type) {
+    return new SimpleInfo(type);
+}
+
+FunctionInfo* f_info(VectorOfTypes parameter_types, TypeNode* return_type) {
+    return new FunctionInfo(parameter_types, return_type);
+}
+
+
+SymbolInfo* w_sinfo(std::string type) {
+    SymbolInfo* ginfo = new SymbolInfo;
+    ginfo->type = SINFO::SIMPLE;
+    SimpleInfo* sinfo = new SimpleInfo(i_type(type, {}));
+    ginfo->simple_info = sinfo;
+    return ginfo;
+}
+
+SymbolInfo* w_finfo(VectorOfTypes parameter_types, TypeNode* return_type) {
+    SymbolInfo* ginfo = new SymbolInfo;
+    ginfo->type = SINFO::FUNCTION;
+    FunctionInfo* finfo = new FunctionInfo(parameter_types, return_type);
+    ginfo->function_info = finfo;
+    return ginfo;
+}
+
+typedef std::map<std::string, SimpleInfo*> MapStringToSimple;
+typedef std::map<std::string, FunctionInfo*> MapStringToFunction;
+
+
+SymbolInfo* w_cinfo(MapStringToSimple fields, MapStringToFunction methods) {
+    SymbolInfo* ginfo = new SymbolInfo;
+    ginfo->type = SINFO::CLASS;
+    ClassInfo* cinfo = new ClassInfo;
+    cinfo->methods = methods;
+    cinfo->fields = fields;
+    ginfo->class_info = cinfo;
+    return ginfo;
+}
+
 VectorOfNodes get_tree(std::string text) {
     Scanner scanner(text);
     std::vector<Token> tokens = scanner.scan_all();
@@ -28,16 +132,17 @@ TEST(semantic_test, fun_foo) {
     EXPECT_EQ(ginfo->type, SINFO::FUNCTION);
 }
 
+TypeNode* typenode_integer = i_type("Integer", {});
+TypeNode* typenode_string = i_type("String", {});
+TypeNode* typenode_boolean = i_type("Boolean", {});
+
 TEST(semantic_test, fun_foo_eq) {
     std::string text = "fun foo(){}";
     VectorOfNodes tree = get_tree(text);
     FirstPass fp;
     fp.analyze(tree);
     SymbolInfo* ginfo = fp.globals->get("foo");
-    EXPECT_EQ(ginfo->type, SINFO::FUNCTION);
-    FunctionInfo* finfo = ginfo->function_info;
-    EXPECT_EQ(finfo->parameter_types.size(), 0);
-    EXPECT_EQ(finfo->return_type, nullptr);
+    EXPECT_TRUE(equal(ginfo, w_finfo({}, nullptr)));
 }
 
 TEST(semantic_test, fun_foo_complete) {
@@ -46,19 +151,7 @@ TEST(semantic_test, fun_foo_complete) {
     FirstPass fp;
     fp.analyze(tree);
     SymbolInfo* ginfo = fp.globals->get("foo");
-    FunctionInfo* finfo = ginfo->function_info;
-    EXPECT_EQ(ginfo->type, SINFO::FUNCTION);
-
-    EXPECT_EQ(finfo->parameter_types.size(), 2);
-    SimpleInfo* yinfo = finfo->parameter_types[0];
-    EXPECT_EQ(yinfo->parent, "Integer");
-    EXPECT_EQ(yinfo->type_parameters.size(), 0);
-    SimpleInfo* xinfo = finfo->parameter_types[1];
-    EXPECT_EQ(xinfo->parent, "String");
-    EXPECT_EQ(xinfo->type_parameters.size(), 0);
-    SimpleInfo* ret_info = finfo->return_type;
-    EXPECT_EQ(ret_info->parent, "Boolean");
-    EXPECT_EQ(ret_info->type_parameters.size(), 0);
+    EXPECT_TRUE(equal(ginfo, w_finfo({typenode_integer, typenode_string}, typenode_boolean)));
 }
 
 TEST(semantic_test, class_foo) {
@@ -76,9 +169,7 @@ TEST(semantic_test, class_foo_eq) {
     FirstPass fp;
     fp.analyze(tree);
     SymbolInfo* ginfo = fp.globals->get("Foo");
-    ClassInfo* cinfo = ginfo->class_info;
-    EXPECT_EQ(cinfo->methods.size(), 0);
-    EXPECT_EQ(cinfo->fields.size(), 0);
+    EXPECT_TRUE(equal(ginfo, w_cinfo(MapStringToSimple(), MapStringToFunction())));
 }
 
 TEST(semantic_test, class_foo_with_field) {
@@ -87,12 +178,9 @@ TEST(semantic_test, class_foo_with_field) {
     FirstPass fp;
     fp.analyze(tree);
     SymbolInfo* ginfo = fp.globals->get("Foo");
-    ClassInfo* cinfo = ginfo->class_info;
-    EXPECT_EQ(cinfo->methods.size(), 0);
-    EXPECT_EQ(cinfo->fields.size(), 1);
-    SimpleInfo* xinfo = cinfo->fields["x"];
-    EXPECT_EQ(xinfo->parent, "String");
-    EXPECT_EQ(xinfo->type_parameters.size(), 0);
+    MapStringToSimple fields;
+    fields["x"] = s_info(typenode_string);
+    EXPECT_TRUE(equal(ginfo, w_cinfo(fields, MapStringToFunction())));
 }
 
 TEST(semantic_test, class_foo_with_method) {
@@ -101,15 +189,7 @@ TEST(semantic_test, class_foo_with_method) {
     FirstPass fp;
     fp.analyze(tree);
     SymbolInfo* ginfo = fp.globals->get("Foo");
-    ClassInfo* cinfo = ginfo->class_info;
-    EXPECT_EQ(cinfo->methods.size(), 1);
-    EXPECT_EQ(cinfo->fields.size(), 0);
-    FunctionInfo* foo_info = cinfo->methods["foo"];
-    EXPECT_EQ(foo_info->parameter_types.size(), 1);
-    SimpleInfo* xinfo = foo_info->parameter_types[0];
-    EXPECT_EQ(xinfo->parent, "Integer");
-    EXPECT_EQ(xinfo->type_parameters.size(), 0);
-    SimpleInfo* ret_info = foo_info->return_type;
-    EXPECT_EQ(ret_info->parent, "String");
-    EXPECT_EQ(ret_info->type_parameters.size(), 0);
+    MapStringToFunction methods;
+    methods["foo"] = f_info({typenode_integer}, {});
+    EXPECT_TRUE(equal(ginfo, w_cinfo(MapStringToSimple(), methods)));
 }
