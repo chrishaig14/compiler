a9b93eb60588476e3c0cd60fcfa791d028942b7a
Christian Pedersen <christian.pedersen@jampp.com>
2020-07-18 22:24:42 -0300

    Add missing return types to test functions

--- test/second_pass_test.cpp
+++ test/second_pass_test.cpp
@@ -102,17 +102,17 @@ TEST(semantic_test, z_not_found_error) {
 }
 
 TEST(semantic_test, x_redeclare_in_inner_scope_ok) {
-    std::string text = "fun foo(y: Foo)->String{var x:Integer;if(y==1){if(y==2){var x:Integer; return x;}}}";
+    std::string text = "fun foo(y: Foo)->Integer{var x:Integer;if(y==1){if(y==2){var x:Integer; return x;}}}";
     ASSERT_OK();
 }
 
 TEST(semantic_test, x_redeclare_in_same_scope_error) {
-    std::string text = "fun foo(y: Foo)->String{var x:Integer;return x;var x:String;}";
+    std::string text = "fun foo(y: Foo)->String{var x:Integer;var x:String; return x;}";
     ASSERT_THROWS_REDECLARED_ERROR("x");
 }
 
 TEST(semantic_test, x_declare_in_inner_scope_and_use_outside_error) {
-    std::string text = "fun foo(y: Foo)->String{if(y==1){if(y==2){var x:Integer;return x;}}return x;}";
+    std::string text = "fun foo(y: Foo)->Integer{if(y==1){if(y==2){var x:Integer;return x;}}return x;}";
     ASSERT_THROWS_NOT_FOUND_ERROR("x");
 }
 
@@ -122,12 +122,12 @@ TEST(semantic_test, member_without_this_error) {
 }
 
 TEST(semantic_test, member_with_this_no_error) {
-    std::string text = "class Foo{var x: String; fun foo(){return this.x;}}";
+    std::string text = "class Foo{var x: String; fun foo()->String{return this.x;}}";
     ASSERT_OK();
 }
 
 TEST(semantic_test, member_not_found_error) {
-    std::string text = "class Foo{var x: String; fun foo(){return this.y;}}";
+    std::string text = "class Foo{var x: String; fun foo()->String{return this.y;}}";
     ASSERT_THROWS_NOT_FOUND_ERROR("y");
 }
 
