517dc51a00f52008e2f69a85baaac4152710065f
Christian Pedersen <christian.pedersen@jampp.com>
2020-08-09 20:37:50 -0300

    A couple more tests

--- test/total_test.cpp
+++ test/total_test.cpp
@@ -72,3 +72,52 @@ TEST(total_test, test_factorial) {
     code_runner.run();
     EXPECT_TRUE(stack.top()->equal(new IntegerObject(3628800)));
 }
+
+TEST(total_test, test_3) {
+    std::string text = "struct Person { name: String; age: Integer;} fun get_name(p: Person)->String {return p.name;}var chris = Person{name:\"Hello\",age:24}; var foo = get_name(chris);";
+    Scanner scanner(text);
+    std::vector<Token> tokens = scanner.scan_all();
+    Parser parser(tokens);
+    BlockNode* program = parser.parse_program();
+    GlobalProcessor gp;
+    gp.visit(*program);
+    Checker checker(gp.globals, gp.class_table);
+    checker.visit(*program);
+    Translator translator;
+    program->accept(translator);
+    Code translated_code = translator.code;
+    ObjectStack stack;
+    StructProtos structs;
+    CodeRunner code_runner(translated_code, structs, stack, {});
+    code_runner.run();
+    auto expected_object = new UserObject("Person", {"name", "age"});
+    expected_object->fields["name"] = new StringObject("Hello");
+    expected_object->fields["age"] = new IntegerObject(24);
+    EXPECT_TRUE(code_runner.env->get("chris")->equal(expected_object));
+    EXPECT_TRUE(code_runner.env->get("foo")->equal(new StringObject("Hello")));
+}
+
+TEST(total_test, test_4) {
+    std::string text = "struct Person { name: String; age: Integer;} fun get_name(p: Person)->String {return p.name;}var chris = Person{name:\"Hello\",age:24}; chris.name = \"Alex\";var foo = get_name(chris);";
+    Scanner scanner(text);
+    std::vector<Token> tokens = scanner.scan_all();
+    Parser parser(tokens);
+    BlockNode* program = parser.parse_program();
+    GlobalProcessor gp;
+    gp.visit(*program);
+    Checker checker(gp.globals, gp.class_table);
+    checker.visit(*program);
+    Translator translator;
+    program->accept(translator);
+    Code translated_code = translator.code;
+    ObjectStack stack;
+    StructProtos structs;
+    CodeRunner code_runner(translated_code, structs, stack, {});
+    code_runner.run();
+    auto expected_object = new UserObject("Person", {"name", "age"});
+    expected_object->fields["name"] = new StringObject("Alex");
+    expected_object->fields["age"] = new IntegerObject(24);
+    EXPECT_TRUE(code_runner.env->get("chris")->equal(expected_object));
+    EXPECT_TRUE(code_runner.env->get("foo")->equal(new StringObject("Alex")));
+}
+
