938703400b643290228ea5744399ad599cb713d0
Christian Pedersen <christian.pedersen@jampp.com>
2020-10-13 21:08:20 -0300

    Add tests for generic functon

--- test/full_test/should_fail/call_complex_generic_function_error.pl
+++ test/full_test/should_fail/call_complex_generic_function_error.pl
@@ -1,7 +1,13 @@
-fun foo(x: a, y: a)->a{
-    return x;
+fun imap(l: List[t], f: fun(t)->b) -> List[b] {
+    return [f(l[0])];
+}
+
+fun foo(x: String) -> String {
+    return "Hello";
 }
 
 fun main()->Integer{
-    return foo(7,"Hello");
+    var f : List[String] = imap([5,4,6],foo);
+    print(f[0]);
+    return 0;
 }
