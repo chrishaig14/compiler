3c9aa3e85abcd5f43cc3abbb74170ac67d4a9fac
Christian Pedersen <christian.pedersen@jampp.com>
2020-11-29 14:22:14 -0300

    Log parsing/checking/translating times

--- src/runner.cpp
+++ src/runner.cpp
@@ -7,7 +7,10 @@
 #include "translator/Translator.h"
 #include "vm/Loader.h"
 #include "vm/CodeRunner.h"
-
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <chrono>
+#include <spdlog/stopwatch.h>
 
 void compile_and_run(std::string text) {
     Scanner scanner(text);
@@ -19,7 +22,9 @@ void compile_and_run(std::string text) {
     Parser parser(tokens);
     BlockNode* program;
     try {
+        auto parse_stopwatch = spdlog::stopwatch();
         program = parser.parse_program();
+        spdlog::get("main")->info("{:<32}\tTotal time: {:03.9f} seconds", "Finished parsing", parse_stopwatch);
     } catch (const UnexpectedToken& ut) {
         std::cerr << ut.what() << std::endl;
         exit(1);
@@ -28,12 +33,17 @@ void compile_and_run(std::string text) {
     Translator translator;
 
     try {
+        auto check_stopwatch = spdlog::stopwatch();
         GlobalProcessor gp(builtins);
         gp.visit(*program);
         Checker checker(gp.globals, gp.class_table, gp.function_table);
         checker.visit(*program);
+        auto end = std::chrono::steady_clock::now();
+        spdlog::get("main")->info("{:<32}\tTotal time: {:03.9f} seconds", "Finished checking", check_stopwatch);
 //        program->accept(translator);
+        auto translate_stopwatch = spdlog::stopwatch();
         translator.visit(*program);
+        spdlog::get("main")->info("{:<32}\tTotal time: {:03.9f} seconds", "Finished translating", translate_stopwatch);
     } catch (const std::runtime_error& e) {
         std::cerr << "THERE WAS A SEMANTIC ERROR: " << e.what() << std::endl;
         exit(1);
@@ -67,5 +77,7 @@ int main(int argc, char* argv[]) {
     std::stringstream sstream;
     sstream << file.rdbuf();
     std::string text = sstream.str();
+    spdlog::set_level(spdlog::level::debug);
+    spdlog::stdout_color_mt("main");
     compile_and_run(text);
 }
