20c2fc95a3db439435330eacf28676866259863a
Christian Pedersen <christian.pedersen@jampp.com>
2021-02-15 02:59:50 -0300

    new for dict

--- src/transpiler/Transpiler.cpp
+++ src/transpiler/Transpiler.cpp
@@ -518,14 +518,14 @@ std::string Transpiler::visit_list(ListNode& node) {
 
 std::string Transpiler::visit_dict(DictNode& node) {
     std::string out;
-    out = "GC::register_object((new XDict(std::unordered_map<XObject*,XObject*>({";
+    out = "NEW(XDict,std::unordered_map<XObject*,XObject*>({";
     for (int i = 0; i < node.items.size(); i++) {
         out += "{" + this->dispatch(node.items[i].first) + ", " + this->dispatch(node.items[i].second) + "}, ";
     }
     if (node.items.size() != 0) {
         out = out.substr(0, out.size() - 2);
     }
-    out += "}))))";
+    out += "}))";
     return out;
 }
 
