814be66a90de33759a54d19fb69ead927f9b0f42
Christian Pedersen <christian.pedersen@jampp.com>
2021-04-12 16:45:34 -0300

    add whilesnode

---
+++ src/simple_nodes/WhileSNode.cpp
@@ -0,0 +1,8 @@
+//
+// Created by chris on 10/4/21.
+//
+
+#include "WhileSNode.h"
+
+WhileSNode::WhileSNode() : SNode(SNodeType::WHILE) {
+}
---
+++ src/simple_nodes/WhileSNode.h
@@ -0,0 +1,21 @@
+//
+// Created by chris on 10/4/21.
+//
+
+#ifndef XLANG_WHILESNODE_H
+#define XLANG_WHILESNODE_H
+
+
+#include "SNode.h"
+#include "BlockSNode.h"
+
+class WhileSNode : public SNode {
+public:
+    SNode* condition;
+    BlockSNode* body;
+
+    WhileSNode();
+};
+
+
+#endif //XLANG_WHILESNODE_H
