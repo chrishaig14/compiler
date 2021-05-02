d73c4f80c91fc8d97ddf591b2bfe9cb87fa00541
Christian Pedersen <christian.pedersen@jampp.com>
2021-05-02 02:11:16 -0300

    simplify entity

--- src/units/Entity.h
+++ src/units/Entity.h
@@ -62,8 +62,22 @@ struct Value {
 enum class E_TYPE {
     PACKAGE, MODULE, CLASS, FUNCTION_VALUE, OBJECT_VALUE, CONST_FUNCTION, ERROR, NOT_FOUND, ENUM
 };
+//
+// struct Entity {
+//     E_TYPE type;
+//     union {
+//         Package* package;
+//         Module* module;
+//         Class* clazz;
+//         FunctionValue* function_value;
+//         ObjectValue* object_value;
+//         ConstFunction* const_function;
+//         Enum* enumm;
+//     };
+// };
 
-struct Entity {
+class Entity {
+public:
     E_TYPE type;
     union {
         Package* package;
@@ -74,6 +88,33 @@ struct Entity {
         ConstFunction* const_function;
         Enum* enumm;
     };
+
+    Entity() : type(E_TYPE::NOT_FOUND), package(nullptr) {
+    }
+
+    Entity(E_TYPE type) : type(type), package(nullptr) {
+    }
+
+    Entity(Package* package) : type(E_TYPE::PACKAGE), package(package) {
+    }
+
+    Entity(Module* module) : type(E_TYPE::MODULE), module(module) {
+    }
+
+    Entity(Class* clazz) : type(E_TYPE::CLASS), clazz(clazz) {
+    }
+
+    Entity(FunctionValue* function_value) : type(E_TYPE::FUNCTION_VALUE), function_value(function_value) {
+    }
+
+    Entity(ObjectValue* object_value) : type(E_TYPE::OBJECT_VALUE), object_value(object_value) {
+    }
+
+    Entity(ConstFunction* const_function) : type(E_TYPE::CONST_FUNCTION), const_function(const_function) {
+    }
+
+    Entity(Enum* enumm) : type(E_TYPE::ENUM), enumm(enumm) {
+    }
 };
 
 std::string flirpintype_to_str(F_TYPE flirpintype);
