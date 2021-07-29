be1199c4b31228a679b727fbad48fc110a0c4233
Christian Pedersen <christian.pedersen@jampp.com>
2021-07-29 17:43:51 -0300

    remove unimplemented method in Checker

--- src/semantic/Checker.h
+++ src/semantic/Checker.h
@@ -163,8 +163,6 @@ public:
     void fill_value(Value* value);
     std::unique_ptr<SemanticInfo> expect_rvalue_of_type(const TypeNode& target, Node& node);
     void process_function_arguments(SemanticInfo& retv, std::vector<Entity>& arg_entities, CallSNode* sn, CallNode& n,
-                                    FunctionType* p_type, SemanticInfo& ptr);
-    void process_function_arguments(SemanticInfo& retv, std::vector<Entity>& arg_entities, CallSNode* sn, CallNode& n,
                                     FunctionType* function_type, SemanticInfo* fun_info_p);
     bool check_arguments(CallNode& n, CallSNode* sn, VectorOfTypes& arg_types, std::vector<Entity>& arg_entities);
     USemanticInfo
