039e691e100b736a79304e59cd3d4e1b89c49eb1
Christian Pedersen <christian.pedersen@jampp.com>
2021-07-29 17:20:03 -0300

    remove rel_path, classes, functions from Module

--- src/units/Module.h
+++ src/units/Module.h
@@ -22,7 +22,6 @@ public:
     BlockSNode* sast;
 
     std::map<std::string, std::string> included_module_paths;
-    std::string rel_path;
     bool is_lib;
     std::map<std::string, Path> imported_paths_no_alias;
     std::map<std::string, Path> imported_paths_with_alias;
@@ -31,8 +30,6 @@ public:
     std::vector<std::pair<std::string, Path>> imported_paths_with_alias_v;
     std::map<std::string, Entity> imports;
     Path path;
-    std::map<std::string, Class*> classes;
-    std::map<std::string, ConstFunction*> functions;
     std::map<std::string, Flirpin> flirpins;
     std::string full_header_path;
 
