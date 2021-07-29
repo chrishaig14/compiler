8b745c67b9b8d5e748d262d7117500d2f22de05f
Christian Pedersen <christian.pedersen@jampp.com>
2021-07-29 12:42:54 -0300

    fix update_core.sh directories

--- update_core.sh
+++ update_core.sh
@@ -1,6 +1,6 @@
-cd /home/chris/CLionProjects/compiler/lib/core/1.0.0/build
+cd ~/CLionProjects/compiler/lib/core/1.0.0/build
 cmake ../out
 make -j
-cd /home/chris/CLionProjects/compiler/lib/build/
+cd ~/CLionProjects/compiler/lib/build/
 rm libcore*
-ln /home/chris/CLionProjects/compiler/lib/core/1.0.0/build/libcore-1.0.0.a
+ln ~/CLionProjects/compiler/lib/core/1.0.0/build/libcore-1.0.0.a
