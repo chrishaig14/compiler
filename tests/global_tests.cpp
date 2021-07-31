baf2c3dd715c9040a05d0ec12aeb7b2c9cf61aa5
Christian Pedersen <christian.pedersen@jampp.com>
2021-07-31 12:20:44 -0300

    global_tests

--- tests/global_tests.cpp
+++ tests/global_tests.cpp
@@ -6,7 +6,7 @@
 
 const ObjectType NO_TYPE(".None");
 
-TEST_CASE("global", "[parser]") {
+TEST_CASE("global_main", "[parser]") {
     Scanner scanner;
     std::string code = "fun main()->Integer{return 0;}";
     scanner.load_text(code);
@@ -19,4 +19,27 @@ TEST_CASE("global", "[parser]") {
     gp.visit_root(*module.ast);
     REQUIRE(module.flirpins.size() == 1);
     REQUIRE(module.flirpins.count("main") == 1);
+    Flirpin e_flirpin;
+    e_flirpin.type = F_TYPE::CONST_FUNCTION;
+    REQUIRE(module.flirpins["main"].type == e_flirpin.type);
+    REQUIRE(*module.flirpins["main"].const_function->ft == FunctionType({}, new ObjectType("Integer")));
+}
+
+TEST_CASE("global_class", "[parser]") {
+    Scanner scanner;
+    std::string code = "class Foo {}";
+    scanner.load_text(code);
+    std::vector<Token> tokens = scanner.scan_all();
+    Parser parser("test", scanner.code_lines, tokens);
+    parser.top_package_name = "main";
+    Module module(Path("main.foo"), "foo.xl", ".", false, "main.h");
+    module.ast = parser.parse_program();
+    GlobalProcessor gp(&module);
+    gp.visit_root(*module.ast);
+    REQUIRE(module.flirpins.size() == 1);
+    REQUIRE(module.flirpins.count("Foo") == 1);
+    Flirpin e_flirpin;
+    e_flirpin.type = F_TYPE::CLASS;
+    REQUIRE(module.flirpins["Foo"].type == e_flirpin.type);
+    // REQUIRE(*module.flirpins["Foo"].clazz);
 }
