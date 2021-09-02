33b787cfcaa52c538eb04fd877383528b93fadb0
Christian Pedersen <christian.pedersen@jampp.com>
2021-09-01 23:00:16 -0300

    More USNode

--- src/semantic/CheckDeclaration.cpp
+++ src/semantic/CheckDeclaration.cpp
@@ -64,7 +64,7 @@ USNode Checker::make_rvalue(const Entity& t_entity, sem::SNode* value_snode, con
 
         const std::string& unaliased_target_type_id = unaliased_target_type->object().id;
         if (unaliased_target_type_id == "Union") {
-            return USNode(make_union_rvalue(value_snode, unaliased_value_type, unaliased_target_type));
+            return USNode(make_union_rvalue(USNode(value_snode), unaliased_value_type, unaliased_target_type));
         }
 
         if (unaliased_target_type_id == "Option") {
@@ -95,11 +95,11 @@ sem::SNode* Checker::make_option_rvalue(sem::SNode* value_snode, const TypeNode*
     return nullptr;
 }
 
-USNode Checker::make_union_rvalue(sem::SNode* value_snode, const TypeNode* unaliased_value_type,
+USNode Checker::make_union_rvalue(USNode value_snode, const TypeNode* unaliased_value_type,
                                   const TypeNode* unaliased_target_type) const {
     int union_index = target_union_type(unaliased_target_type->object(), unaliased_value_type->object());
     if (union_index != -1) {
-        return USNode(make_union_wrapper(union_index, value_snode));
+        return USNode(make_union_wrapper(union_index, value_snode.release()));
     } else {
         if (unaliased_value_type->object().id == "Union") {
             if (unaliased_value_type->object().type_params.size() <=
@@ -115,7 +115,7 @@ USNode Checker::make_union_rvalue(sem::SNode* value_snode, const TypeNode* unali
                         return nullptr;
                     }
                 }
-                return USNode(value_snode);
+                return value_snode;
             }
         }
         return nullptr;
