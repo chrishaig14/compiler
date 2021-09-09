05c5bd93fdd96010fe71e3c413b2c3a0337ca1cc
Christian Pedersen <christian.pedersen@jampp.com>
2021-09-09 01:05:58 -0300

    leaks

--- src/semantic/CheckCall.cpp
+++ src/semantic/CheckCall.cpp
@@ -208,8 +208,8 @@ bool Checker::check_arguments(ast::Call& n, std::vector<USNode>& arguments, ast:
         }
 
 
-        ast::Type& arg_type = *get_entity_type(arg_entity);
-        arg_types.push_back(arg_type.clone());
+        ast::Type* arg_type = get_entity_type(arg_entity);
+        arg_types.push_back(arg_type);
         // n.arg_types.push_back(arg_type.clone());
     }
     return has_error;
