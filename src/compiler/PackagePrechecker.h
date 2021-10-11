57e0ab9a90f7b1e71bd011a17bdc4202ccaeed92
Christian Pedersen <christian.pedersen@jampp.com>
2021-10-11 20:16:46 -0300

    PackagePrechecker

--- src/compiler/PackagePrechecker.h
+++ src/compiler/PackagePrechecker.h
@@ -14,37 +14,9 @@
 
 class PackagePrechecker {
 public:
-    bool preprocess_module(Module& module) {
-        ModulePrechecker gp(module);
-        std::cout << "** Global-processing module " << E_INFO(module.name) << " at path: " << E_INFO(module.abs_path)
-                  << std::endl;
-        gp.visit_root();
-        return gp.error_reporter.ok();
-    }
+    bool preprocess_module(Module& module);
 
-    bool preprocess_package(Package& package) {
-        bool ok = true;
-        std::cout << "* Global-processing package " << E_INFO(package.name) << " at path: " << E_INFO(package.abs_path)
-                  << std::endl;
-        for (const auto& ep: package.units) {
-            Unit* uvalue = ep.second.get();
-            if (uvalue->is_package()) {
-                Package& subpackage = uvalue->package();
-                if (not this->preprocess_package(subpackage)) {
-                    std::cout << E_HLT("Error global processing package ") << E_INFO(subpackage.abs_path) << std::endl;
-                    ok = false;
-                }
-            } else if (uvalue->is_module()) {
-                Module& module = uvalue->module();
-                if (not this->preprocess_module(module)) {
-                    std::cout << E_HLT("Error global processing module ") << E_INFO(module.abs_path) << std::endl;
-                    ok = false;
-                }
-            }
-        }
-        return ok;
-    }
-
+    bool preprocess_package(Package& package);
 };
 
 
