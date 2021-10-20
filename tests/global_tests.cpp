d2ff5eaa90b947d463dff1bccee97dea1a5f3478
Christian Pedersen <christian.pedersen@jampp.com>
2021-10-19 21:58:12 -0300

    global tests cleanup

--- tests/global_tests.cpp
+++ tests/global_tests.cpp
@@ -7,39 +7,48 @@
 
 const ast::ObjectType NO_TYPE(".None");
 
-TEST_CASE("global_main", "[parser]") {
-    Scanner scanner;
-    std::string code = "fun main()->Integer{return 0;}";
-    scanner.load_text(code);
-    std::vector<Token> tokens = scanner.scan_all();
-    Parser parser("test", scanner.code_lines, tokens);
-    Module module(Path("main.foo"), "foo.xl", false);
-    module.ast = parser.parse_module();
-    std::map<std::string, std::string> instances;
-    ModulePrechecker gp(module, instances);
-    gp.visit_root();
+class ModulePrecheckerTest {
+public:
+    Module module;
 
+    ModulePrecheckerTest(const std::string& code) : module(Path("main.foo"), "foo.xl", false) {
+        Scanner scanner;
+        scanner.load_text(code);
+        std::vector<Token> tokens = scanner.scan_all();
+        Parser parser("test", scanner.code_lines, tokens);
+        module.ast = parser.parse_module();
+        std::map<std::string, std::string> instances;
+        ModulePrechecker gp(module, instances);
+        gp.visit_root();
+    }
+};
+
+TEST_CASE("global_main", "[parser]") {
+    ModulePrecheckerTest ct("fun main()->Integer{return 0;}");
+    Module& module = ct.module;
     REQUIRE(module.members.size() == 1);
     REQUIRE(module.members.count("main") == 1);
     REQUIRE(module.members["main"]->is_const_function());
     ConstFunction* const_function = &module.members["main"]->const_function();
-    // sem::Type* p = (sem::TypeObject*) nullptr;
     std::unique_ptr<sem::Type> u = std::make_unique<sem::TypeObject>("Integer");
     REQUIRE(const_function->const_function_ft == sem::TypeFunction({}, std::move(u)));
     REQUIRE(const_function->path.as_str() == "main.foo.main");
 }
 
 TEST_CASE("global_class", "[parser]") {
-    Scanner scanner;
-    std::string code = "class Foo {x:Integer;y: String; fun foo(w: String)->Integer{return 0;} static fun static_foo(x: Boolean)->String{return \"Hello\";}}";
-    scanner.load_text(code);
-    std::vector<Token> tokens = scanner.scan_all();
-    Parser parser("test", scanner.code_lines, tokens);
-    Module module(Path("main.foo"), "foo.xl", false);
-    module.ast = parser.parse_module();
-    std::map<std::string, std::string> instances;
-    ModulePrechecker gp(module,instances);
-    gp.visit_root();
+    ModulePrecheckerTest ct(R"(
+class Foo {
+    x:Integer
+    y: String
+    fun foo(w: String)->Integer{
+        return 0
+    }
+    static fun static_foo(x: Boolean)->String{
+        return "Hello"
+    }
+}
+)");
+    Module& module = ct.module;
 
     REQUIRE(module.members.size() == 1);
     REQUIRE(module.members.count("Foo") == 1);
@@ -76,16 +85,16 @@ TEST_CASE("global_class", "[parser]") {
 }
 
 TEST_CASE("global_multiple", "[parser]") {
-    Scanner scanner;
-    std::string code = "import moduleA; fun main()->Integer{return 0;}\nclass Foo {x: Integer;}\n";
-    scanner.load_text(code);
-    std::vector<Token> tokens = scanner.scan_all();
-    Parser parser("test", scanner.code_lines, tokens);
-    Module module(Path("main.foo"), "foo.xl", false);
-    module.ast = parser.parse_module();
-    std::map<std::string, std::string> instances;
-    ModulePrechecker gp(module,instances);
-    gp.visit_root();
+    ModulePrecheckerTest ct(R"(
+import moduleA
+fun main()->Integer{
+    return 0
+}
+class Foo {
+    x: Integer
+}
+)");
+    Module& module = ct.module;
 
     REQUIRE(module.members.size() == 2);
     REQUIRE(module.members.count("main") == 1);
